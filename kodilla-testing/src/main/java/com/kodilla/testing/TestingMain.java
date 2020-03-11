8aa283e0e774919256d950a53353e4c32847c6b2
Filip Timofiejew <ftimofiejew@gmail.com>
2020-03-11 20:59:31 +0100

    Calculator tests added

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -4,29 +4,25 @@ import com.kodilla.testing.calculator.Calculator;
 import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         SimpleUser simpleUser = new SimpleUser("theForumUser");
 
         String result = simpleUser.getUsername();
 
-        if (result.equals("theForumUser")){
+        if (result.equals("theForumUser")) {
             System.out.println("test OK");
         } else {
             System.out.println("Error!");
         }
 
-        System.out.println("Pierwszy test jednostkowy: ");
-
         Calculator calculator = new Calculator();
-        if(calculator.add(2, 6) == 2 + 6){
-            System.out.println("test add() OK");
+
+        int addResult = calculator.add(10, 15);
+        int subResult = calculator.subtract(100, 75);
+        if(addResult == 25 && subResult == 25){
+            System.out.println("calculator test OK");
         } else {
-            System.out.println("Error!");
-        }
-        if(calculator.subtract(10, 2) == 10 -2){
-            System.out.println("test subtract() OK");
-        } else {
-            System.out.println("Error!");
+            System.out.println("calculator Error!");
         }
 
     }
