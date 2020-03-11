b0c12264e9ac58cba3cac465fd04912cbdc49d7d
Filip Timofiejew <ftimofiejew@gmail.com>
2020-03-11 13:57:29 +0100

    adding tests

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,9 +1,33 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
 
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania.");
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Pierwszy test jednostkowy: ");
+
+        Calculator calculator = new Calculator();
+        if(calculator.add(2, 6) == 2 + 6){
+            System.out.println("test add OK");
+        } else {
+            System.out.println("Error!");
+        }
+        if(calculator.subtract(10, 2) == 10 -2){
+            System.out.println("test subtract OK");
+        } else {
+            System.out.println("Error!");
+        }
 
     }
 }
