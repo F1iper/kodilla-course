a1df9f983b8edfe1cd9575f19087e20eb1b13c91
Filip Timofiejew <ftimofiejew@gmail.com>
2020-06-07 19:49:52 +0200

    16.6 JDBC test added

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -1,5 +1,6 @@
 package com.kodilla.jdbc;
 
+import com.mysql.cj.protocol.Resultset;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -41,5 +42,55 @@ public class DbManagerTestSuite {
         rs.close();
         statement.close();
         Assert.assertEquals(5, counter);
+    }
+
+    @Test
+    public void testSelectPosts() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+
+        //When
+        String sqlQuery = "SELECT * FROM ISSUES";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while(rs.next()) {
+            System.out.println(rs.getInt("ID") + ", " +
+                    rs.getString("SUMMARY") + ", " +
+                    rs.getString("DESCRIPTION"));
+            counter++;
+        }
+        rs.close();
+        statement.close();
+        Assert.assertEquals(15, counter);
+    }
+
+    @Test
+    public void testSelectUsersAndPosts() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+
+        //When
+        String sqlQuery = "select u.firstname, u.lastname, count(*) as posts_number from users u\n" +
+                "join posts p on u.id = p.user_id\n" +
+                "group by u.id\n" +
+                "having count(*) >= 2;\n" +
+                "\n";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while(rs.next()) {
+            System.out.println(rs.getString("firstname") + ", " +
+                    rs.getString("lastname") + ", " +
+                    rs.getInt("posts_number"));
+            counter++;
+        }
+        rs.close();
+        statement.close();
+        Assert.assertEquals(3, counter);
     }
 }
